41e93e37aa6b48334f3503fdea66081e0e75bc0b
Kourtney Reynolds <kourtneyreynolds@Kourtneys-MBP.xmission.com>
2024-04-15 12:57:19 -0600

    Add in some basic types and parse the birth_rates file

--- bin/save-births.ts
+++ bin/save-births.ts
@@ -1,18 +1,30 @@
 import fs from 'node:fs/promises'
-const ALL = 'All Races'
+import * as _ from 'lodash'
 
-type BirthOrigin = 'All Races' |
-    'American Indian or Alaska Native' |
-    'Asian or Pacific Islander' |
-    'Hispanic' |
-    'Non-Hispanic Black' |
-    'Non-Hispanic White' |
-    'Non-Hispanic, Single Race White' |
-    'Non-Hispanic, Single Race Black' |
-    'Non-Hispanic, Single Race American Indian or Alaska Native' |
-    'Non-Hispanic, Single Race Asian' |
-    'Non-Hispanic, Single Race Native Hawaiian or Pacific Islander' |
-    null
+const ALL = 'All Races'
+const AMERICAN_NATIVE =  'American Indian or Alaska Native' 
+const ASIAN_PACIFIC = 'Asian or Pacific Islander' 
+const HISPANIC = 'Hispanic' 
+const NON_HIS_BLACK = 'Non-Hispanic Black' 
+const NON_HIS_WHITE = 'Non-Hispanic White' 
+const NON_HIS_RACE_WHITE = 'Non-Hispanic, Single Race White' 
+const NON_HIS_RACE_BLACK = 'Non-Hispanic, Single Race Black' 
+const NON_HIS_RACE_NATIVE = 'Non-Hispanic, Single Race American Indian or Alaska Native' 
+const NON_HIS_ASIAN = 'Non-Hispanic, Single Race Asian' 
+const NON_HIS_PACIFIC_IL = 'Non-Hispanic, Single Race Native Hawaiian or Pacific Islander' 
+
+
+type BirthOrigin = typeof ALL |
+typeof AMERICAN_NATIVE |
+typeof ASIAN_PACIFIC |
+typeof HISPANIC |
+typeof NON_HIS_BLACK |
+typeof NON_HIS_WHITE |
+typeof NON_HIS_RACE_WHITE |
+typeof NON_HIS_RACE_BLACK |
+typeof NON_HIS_RACE_NATIVE |
+typeof NON_HIS_ASIAN |
+typeof NON_HIS_PACIFIC_IL | null
 
 enum BIRTH_ORIGIN {
     ALL = 'All',
@@ -23,25 +35,28 @@ enum BIRTH_ORIGIN {
     WHITE = 'White'
 } 
 
-class Birth {
-    year: string | null
-    race: BirthOrigin | null
-    age: string | null
-    rate: number | null
-    isBlack: boolean | null
+class BirthStat {
+    year: string 
+    race: BirthOrigin 
+    age: string 
+    rate: number 
+    isBlack: boolean 
 
-    constructor() {
-        this.year = null
-        this.race = null
-        this.age = ''
+    constructor(race: BirthOrigin) {
+        this.year = ''
+        this.race = race
+        this.age = '0'
         this.rate = 0
-        this.isBlack = false
+        this.isBlack = this.isAAOrBlackOrigin()
     }
 
     static originBlack = 'Non-Hispanic Black'
 
-    isAAOrBlackOrigin(race: string) {
-        return this.race === 
+    isAAOrBlackOrigin() {
+        if(this.race?.trim() === '') return false
+        
+        return [NON_HIS_BLACK, NON_HIS_RACE_BLACK].includes(this.race as string)//Look up how to use partials again
+      
     }
 }
 //create a function to run everything
@@ -49,6 +64,37 @@ class Birth {
     //create a variable for the file
     //Use fs to read the file
     const data = await fs.readFile('./birth_rates.csv', { encoding: 'utf-8' })
-    console.log(data)
+    //Remove the first row of data and save as columns
+    const rows = data.split('\n')
+    const cols = rows[0].split(',').map((c: string, idx:number) => {
+        if (c === 'Race or Hispanic Origin') {
+            return 'race'
+        } else if (c === 'Age Group') {
+            return 'age'
+        } else if (c === 'Birth Rate') {
+            return 'rate'
+        }
+        return c.toLowerCase()
+    })
+    const raceCol = 1
+
+    const stats = rows.slice(1).map((row: string) => {
+        const item = row.split(',')
+
+        //Create an instance of birth origin
+        let newStat = new BirthStat(item[raceCol] as BirthOrigin) 
+     
+        //Get the cols and map them into an array set with key/value object
+        const data = cols.map((d: any, idx:number) => {
+            return { [d]: item[idx]}
+        })
+     
+        //Convert the array of objects into one object
+        const birthObject = Object.assign({}, ...data)
+        const result = { ...newStat, ...birthObject }
+        console.log(result.isBlack)
+        console.log(typeof result)
+    })
+
     console.log(typeof data)
 })()
--- dist/bin/save-births.js
+++ dist/bin/save-births.js
@@ -1,1 +1,76 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const promises_1 = __importDefault(require("node:fs/promises"));
+const ALL = 'All Races';
+const AMERICAN_NATIVE = 'American Indian or Alaska Native';
+const ASIAN_PACIFIC = 'Asian or Pacific Islander';
+const HISPANIC = 'Hispanic';
+const NON_HIS_BLACK = 'Non-Hispanic Black';
+const NON_HIS_WHITE = 'Non-Hispanic White';
+const NON_HIS_RACE_WHITE = 'Non-Hispanic, Single Race White';
+const NON_HIS_RACE_BLACK = 'Non-Hispanic, Single Race Black';
+const NON_HIS_RACE_NATIVE = 'Non-Hispanic, Single Race American Indian or Alaska Native';
+const NON_HIS_ASIAN = 'Non-Hispanic, Single Race Asian';
+const NON_HIS_PACIFIC_IL = 'Non-Hispanic, Single Race Native Hawaiian or Pacific Islander';
+var BIRTH_ORIGIN;
+(function (BIRTH_ORIGIN) {
+    BIRTH_ORIGIN["ALL"] = "All";
+    BIRTH_ORIGIN["NATIVE"] = "Native";
+    BIRTH_ORIGIN["ASIAN_PACIFIC"] = "AsianPacific";
+    BIRTH_ORIGIN["HISPANIC"] = "Hispanic";
+    BIRTH_ORIGIN["BLACK"] = "Black";
+    BIRTH_ORIGIN["WHITE"] = "White";
+})(BIRTH_ORIGIN || (BIRTH_ORIGIN = {}));
+class BirthStat {
+    constructor(race) {
+        this.year = '';
+        this.race = race;
+        this.age = '0';
+        this.rate = 0;
+        this.isBlack = false;
+    }
+    isAAOrBlackOrigin() {
+        if (this.race) {
+            return [NON_HIS_BLACK, NON_HIS_RACE_BLACK].includes(this.race);
+        }
+        return false;
+    }
+}
+BirthStat.originBlack = 'Non-Hispanic Black';
+//create a function to run everything
+(async () => {
+    //create a variable for the file
+    //Use fs to read the file
+    const data = await promises_1.default.readFile('./birth_rates.csv', { encoding: 'utf-8' });
+    //Remove the first row of data and save as columns
+    const rows = data.split('\n');
+    const cols = rows[0].split(',').map((c, idx) => {
+        if (c === 'Race or Hispanic Origin') {
+            return 'race';
+        }
+        else if (c === 'Age Group') {
+            return 'age';
+        }
+        else if (c === 'Birth Rate') {
+            return 'rate';
+        }
+        return c.toLowerCase();
+    });
+    const raceCol = 1;
+    const stats = rows.splice(1, 1).map((row) => {
+        const item = row.split(',');
+        let newStat = new BirthStat(item[raceCol]);
+        const data = cols.map((d, idx) => {
+            return { [d]: item[idx] };
+        });
+        const birthObject = Object.assign({}, ...data);
+        const result = Object.assign(Object.assign({}, newStat), birthObject);
+        console.log(result);
+    });
+    console.log(cols);
+    console.log(rows[10]);
+    console.log(typeof data);
+})();
